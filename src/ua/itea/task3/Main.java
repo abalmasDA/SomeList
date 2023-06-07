bb3bc09d1b57700f5054f6d5d9de391c2006f9f3
abalmasDA <abalmasit@gmail.com>
2023-06-07 16:02:40 +0300

    commit1

--- src/ua/itea/task3/Main.java
+++ src/ua/itea/task3/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+/**
+ * Використовуючи Intelij IDEA, створити проект, пакет. Створити клас Main, створити список цілих чисел і за
+ * допомогою ListIterator пройтись по списку і збільшити значення на 1.
+ */
+
+package ua.itea.task3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SomeList someList = new SomeList();
+        someList.addingNumberToList(0);
+        someList.addingNumberToList(1);
+        someList.addingNumberToList(2);
+        someList.addingNumberToList(3);
+        someList.addingNumberToList(4);
+        someList.addingNumberToList(5);
+        someList.addingNumberToList(6);
+        someList.addingNumberToList(7);
+        someList.addingNumberToList(8);
+        someList.addingNumberToList(9);
+        someList.addingNumberToList(10);
+        someList.increasingNumberListByNumber(1);
+        someList.printList();
+
+    }
+
+
 }
--- src/ua/itea/task3/SomeList.java
+++ src/ua/itea/task3/SomeList.java
@@ -1,2 +1,32 @@
-package ua.itea.task3;public class SomeList {
+package ua.itea.task3;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class SomeList {
+    private ArrayList<Integer> someList = new ArrayList<>();
+
+
+    public void addingNumberToList(int numberToAdd) {
+        someList.add(someList.size(), numberToAdd);
+    }
+
+    public void printList() {
+        for (int i = 0; i < someList.size(); i++) {
+            System.out.println(someList.get(i));
+        }
+    }
+
+
+    public void increasingNumberListByNumber(int numberIncrease) {
+        ListIterator<Integer> listIterator = someList.listIterator(0);
+        while (listIterator.hasNext()) {
+            Integer elementOfList = listIterator.next();
+            listIterator.set(elementOfList + numberIncrease);
+        }
+
+
+    }
+
+
 }
